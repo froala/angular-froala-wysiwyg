5873345f4c7c04d3abdebf8e472ca4d90caa3ac9
Stefan Neculai <stefan.neculai@gmail.com>
2017-02-07 18:43:39 +0200

    Add typings to gitignore.

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/core-js/index.d.ts" />
-/// <reference path="globals/jasmine/index.d.ts" />
-/// <reference path="globals/jquery/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
