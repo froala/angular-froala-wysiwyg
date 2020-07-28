d5cf6722663fa2db01771bec7e399c1159921ce4
Quentin DELETTRE <quentin.delettre.neo-soft@externalgcp.ds.airbus.com>
2020-07-28 09:08:43 +0200

    fix: missing implemented interfaces
    OnDestroy & AfterViewInit were missing

--- src/editor/editor.directive.ts
+++ src/editor/editor.directive.ts
@@ -1,5 +1,5 @@
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
-import { Directive, ElementRef, EventEmitter, Input, NgZone, Optional, Output, Renderer, forwardRef } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, Input, NgZone, Output, forwardRef, OnDestroy, AfterViewInit } from '@angular/core';
 
 import FroalaEditor from 'froala-editor';
 
@@ -12,7 +12,7 @@ import FroalaEditor from 'froala-editor';
     multi: true
   }]
 })
-export class FroalaEditorDirective implements ControlValueAccessor {
+export class FroalaEditorDirective implements ControlValueAccessor, OnDestroy, AfterViewInit {
 
   // editor options
   private _opts: any = {
