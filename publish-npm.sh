12d5e3d91344b0aff042e7a8729c561f2608c4d6
Gangadhar <gangadhar.k@CELSYS217-BLR.CELSYSWTC.IN>
2023-05-12 13:18:21 +0530

    Modified npm-publish.sh,deploy-yes

--- publish-npm.sh
+++ publish-npm.sh
@@ -32,8 +32,9 @@ export ANGULAR_EDITOR_NAME=${DEFAULT_NAME}-${TRAVIS_BRANCH}
 jq --arg newval "$ANGULAR_EDITOR_NAME" '.name |= $newval' package.json > tmp.json && mv tmp.json package.json
 wget --no-check-certificate --user ${NEXUS_USER}  --password ${NEXUS_USER_PWD} https://nexus.tools.froala-infra.com/repository/Froala-npm/${PACKAGE_NAME}/-/${PACKAGE_NAME}-${PACKAGE_VERSION}.tgz
 
-npm run build
-cd ./dist
+#npm run build
+./node_modules/@angular/cli/bin/ng build --prod
+cd dist
 jq --arg newval "$ANGULAR_EDITOR_NAME" '.name |= $newval' package.json > tmp.json && mv tmp.json package.json
 jq '.publishConfig |= . + {"registry": "https://nexus.tools.froala-infra.com/repository/Froala-npm/" }' package.json  > new.file && cat new.file > package.json && rm -f new.file
 
